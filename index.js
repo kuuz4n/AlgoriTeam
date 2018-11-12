78c877ce861501d9cd948bde4fd33d8aa6ad49f7
meridius01 <galagronnel@gmail.com>
2018-11-12 14:02:10 +0800

    v1.3

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const nem = requires ('nem-sdk');
-const express = requires ('express');
+const nem = require('nem-sdk').default;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+const port = 3000;
+//let morgan = require('morgan');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('views', path.join(__dirname, 'server/views'));
+app.set('view engine', 'html');
+
+app.listen(port, (err) => {
+    if(err) { return console.error(err); }
+    console.log(`Listening to ${port}...`);
+  });
