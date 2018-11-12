a734e07a89c554629d824dd46a33b94912916fb8
meridius01 <galagronnel@gmail.com>
2018-11-12 14:30:42 +0800

    Added other html files

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
   });
 
+  app.get('/register', (req, res) => {
+    res.sendFile('register.html');
+  });
+
+  app.get('/auction', (req, res) => {
+    res.sendFile('auction.html');
+  });
+
 app.listen(port, (err) => {
     if(err) { return console.error(err); }
     console.log(`Listening to ${port}...`);
