99061732bf2b69cc2d4b9749bf9d748d60509fcd
Johnny Zarate <jklzforyt@gmail.com>
2018-11-13 16:46:27 +0800

    sira pa admin

--- index.js
+++ index.js
@@ -13,16 +13,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
-  });
+  res.sendFile('index.html');
+});
 
-  app.get('/register', (req, res) => {
-    res.sendFile('register.html');
-  });
+app.get('/admin', (req, res) => {
+  res.sendFile(__dirname + '/admin/index.html');
+});
 
-  app.get('/auction', (req, res) => {
-    res.sendFile('auction.html');
-  });
+app.get('/register', (req, res) => {
+  res.sendFile('register.html');
+});
+
+app.get('/auction', (req, res) => {
+  res.sendFile('auction.html');
+});
 
 app.listen(port, (err) => {
     if(err) { return console.error(err); }
