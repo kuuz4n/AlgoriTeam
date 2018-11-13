89c07582ad0bf0c5c3527263f4214ee39fd99e24
meridius01 <galagronnel@gmail.com>
2018-11-13 15:15:59 +0800

    added exports on routers

--- routers/bidRouter.js
+++ routers/bidRouter.js
@@ -3,4 +3,9 @@ const router = express.Router(); //eslint-disable-line
 const SimpleJsonStore = require('simple-json-store');
 
 
-module.export = router;
+
+router.get('/bid', (req, res) => {
+    res.sendFile('bid.html');
+  });
+
+module.exports = router;
--- routers/housenlotRouter.js
+++ routers/housenlotRouter.js
@@ -3,4 +3,8 @@ const router = express.Router(); //eslint-disable-line
 const SimpleJsonStore = require('simple-json-store');
 
 
-module.export = router;
+router.get('/housenlot', (req, res) => {
+    res.sendFile('housenlot.html');
+  });
+
+module.exports = router;
