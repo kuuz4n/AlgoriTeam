9fcc011ca1c96eb6269daf6e16a5d33783ab0073
Johnny Zarate <jklzforyt@gmail.com>
2018-11-13 17:21:32 +0800

    merge error

--- index.js
+++ index.js
@@ -19,30 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/views'));
 
-<<<<<<< HEAD
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-});
-
-app.get('/admin', (req, res) => {
-  res.sendFile(__dirname + '/admin/index.html');
-});
-
-app.get('/register', (req, res) => {
-  res.sendFile('register.html');
-});
-
-app.get('/auction', (req, res) => {
-  res.sendFile('auction.html');
-});
-=======
 app.use('/', indexRouter);
 app.use('/register', registerRouter);
 app.use('/auction', auctionRouter);
 app.use('/housenlot', housenlotRouter);
 app.use('/vehicle', vehicleRouter);
 app.use('/bid', bidRouter);
->>>>>>> f8d893f7edc0d8798ddeabd4902df5ccc235698f
 
 app.listen(port, (err) => {
     if(err) { return console.error(err); }
