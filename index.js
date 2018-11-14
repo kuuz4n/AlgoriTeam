1d40401232f9a07323677c756d2fec20e972fd7c
Johnny Zarate <jklzforyt@gmail.com>
2018-11-14 16:56:13 +0800

    under construction

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 const nemsdk = require('nem-sdk').default;
 const dotenv = require('dotenv');
+const mysql = require('mysql');
 
 const registerRouter = require('./routers/registerRouter');
 const bidRouter = require('./routers/bidRouter');
@@ -12,21 +13,45 @@ const housenlotRouter = require('./routers/housenlotRouter');
 const vehicleRouter = require('./routers/vehicleRouter');
 const auctionRouter = require('./routers/auctionRouter');
 const indexRouter = require('./routers/indexRouter');
+//const adminRouter = require('./routers/adminRouter');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'algoriteam'
+});
 
 dotenv.load();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static('public'));
+
+require("babel-core").transform("code", {
+  plugins: ["transform-es2015-modules-amd"]
+});
+
+app.set('views', path.join(__dirname, ''));
+app.set('scripts', path.join(__dirname, ''));
+app.set('view engine', 'pug');
 
 app.use('/', indexRouter);
+//app.use('/admin', indexRouter);
 app.use('/register', registerRouter);
 app.use('/auction', auctionRouter);
 app.use('/housenlot', housenlotRouter);
 app.use('/vehicle', vehicleRouter);
 app.use('/bid', bidRouter);
 
-app.listen(port, (err) => {
-    if(err) { return console.error(err); }
-    console.log(`Listening to ${port}...`);
+app.get('/admin', function (req, res, next) {
+  connection.query('SELECT * FROM houseandlot', function (error, results, fields) {
+    if (error) throw error;
+    res.render('admin/index.pug', results);
   });
+});
+
+app.listen(port, (err) => {
+  if(err) { return console.error(err); }
+  console.log(`Listening to ${port}...`);
+});
---
+++ public/read.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { houseandlot: [] };
+
+    fetch('http://localhost:3000/admin')
+      .then(resposnse => response.json())
+      .then(houseandlot => (this.setState({houseandlot})))
+  }
+
+    render() {
+        return (
+        <div class="container">
+            <h1>Dashboard</h1>
+            <div class="row d-md-flex flex-md-equal my-md-3 pl-md-3">      
+            <div class="col card">
+                <table class="table">
+                <thead>
+                    <tr>
+                        <td>Lot area (sq.m.)</td>
+                        <td>Floor area (sq.m.)</td>
+                        <td>Address</td>
+                        <td>Year acquired</td>
+                        <td>Condition</td>
+                        <td>Price (PHP)</td>
+                        <td>Actions</td>
+                    </tr>
+                </thead>
+                <tbody>                   
+                    {this.state.houseandlot.map(houseandlotx =>
+                        <tr key={houseandlotx.h_id}>
+                        <td>{houseandlotx.la} </td>
+                        <td>{houseandlotx.fa} </td>
+                        <td>{houseandlotx.address} </td>
+                        <td>{houseandlotx.year_ac}</td>
+                        <td>{houseandlotx.cond}</td>
+                        <td>{houseandlotx.price}</td>
+                        <td><a>Edit</a>|<a>Delete</a></td>
+                        </tr>
+                    )}
+                </tbody>
+                </table>
+            </div>
+            </div>
+        </div>
+        );
+    }
+}
